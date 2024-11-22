89e9cb24417ddcb00c5c4a4b64d47ed4fbfe1150
Nada.cc <84265935+Nadacc@users.noreply.github.com>
2024-11-22 14:33:31 +0530

    Time: 72 ms (15.96%), Space: 50.1 MB (53.45%) - LeetHub

---
+++ 2741-function-composition/2741-function-composition.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    if(functions.length==0){
+    return function(x) {
+        return x; 
+    }
+    }
+    else{
+       
+            return function(x){
+                res=functions[functions.length-1](x);
+                for(let i=functions.length-2;i>=0;i--){
+                res=functions[i](res);
+                
+            }
+            return res;
+        }
+    }
+    
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
