42d36d18afa42763b944c9d462f37e1299732d1d
Nada.cc <84265935+Nadacc@users.noreply.github.com>
2025-01-24 12:10:47 +0530

    Time: 30 ms (24.38%), Space: 53.6 MB (46.86%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    
+    let str1=s.split("").sort().join("");
+    let str2=t.split("").sort().join("");
+    if(str1==str2){
+        return true
+    }
+    else{
+        return false
+    }
+};
